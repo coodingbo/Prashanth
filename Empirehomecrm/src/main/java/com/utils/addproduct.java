5d8dd95a61fb881977dd84276cdc172e0268c763
SMART IT <SMART IT@DESKTOP-7H9P1AP>
2023-10-13 13:05:43 +0530

    stock move

--- Empirehomecrm/src/main/java/com/utils/addproduct.java
+++ Empirehomecrm/src/main/java/com/utils/addproduct.java
@@ -1,5 +1,45 @@
 package com.utils;
 
-public class addproduct {
+import java.io.File;
+import java.io.FileInputStream;
 
-}
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class addproduct {
+		
+		public static String[][] Customerdata(String sheetname) throws Throwable {
+				
+			    File file = new File("C:\\Users\\u\\git\\EmpireHome_Crm\\Empirehomecrm\\src\\main\\java\\com\\testdata\\addproduct.xlsx");
+				FileInputStream stream = new FileInputStream(file);
+				XSSFWorkbook workbook = new XSSFWorkbook(stream);
+				XSSFSheet sheet = workbook.getSheet(sheetname);
+				int rows = sheet.getPhysicalNumberOfRows();
+				int cells = sheet.getRow(1).getLastCellNum();
+				
+				String[][] data = new String[rows-1][cells];
+				
+				for(int i=0;i<rows-1;i++) {
+					for(int j=0;j<cells;j++) {
+						
+						DataFormatter df = new DataFormatter();
+						
+						data[i][j] = df.formatCellValue(sheet.getRow(i+1).getCell(j));
+						
+					}
+				}
+				return data;
+
+		}
+
+		}
+	
+	
+	
+	
+	
+	
+	
+
+
