c8efcb6f8c9f4d6f0b74c3b235477564502a21fc
SMART IT <SMART IT@DESKTOP-7H9P1AP>
2023-10-18 10:40:53 +0530

    customer test

--- Empirehomecrm/src/main/java/com/pageobjects/Customerfunctionality.java
+++ Empirehomecrm/src/main/java/com/pageobjects/Customerfunctionality.java
@@ -1,5 +1,90 @@
 package com.pageobjects;
 
-public class Customerfunctionality {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.base.Basetest;
+
+public class Customerfunctionality extends Basetest {
+
+	@FindBy(xpath = "//span[@class='pe-7s-keypad']")
+	WebElement clickkeypad;
+
+	@FindBy(xpath = "//span[normalize-space()='Customers']")
+	WebElement clickCustomers;
+
+	@FindBy(xpath = "//a[normalize-space()='Add Customer']")
+	WebElement clickAddCustomer;
+
+	@FindBy(xpath = "//input[@placeholder='Enter Customer Name']")
+	WebElement EnterCustomerName;
+
+	@FindBy(xpath = "//input[@placeholder='Enter Mobile Number']")
+	WebElement EnterMobileNumber;
+
+	@FindBy(xpath = "//input[@placeholder='Enter Email Id']")
+	WebElement EnterEmailId;
+
+	@FindBy(xpath = "//input[@placeholder='Enter Whatsapp Number']")
+	WebElement EnterWhatsappNumber;
+
+	@FindBy(xpath = "//button[normalize-space()='Save']")
+	WebElement clickSave;
+
+	@FindBy(xpath = "//input[@class='form-control form-control-sm']")
+	WebElement search;
+
+	@FindBy(xpath = "//tbody/tr[1]/td[11]/a[1]/button[1]")
+	WebElement Edit;
+
+	@FindBy(xpath = "//tbody/tr[1]/td[11]/a[3]/button[1]/i[1]")
+	WebElement Delate;
+
+	@FindBy(xpath = "//button[@class='swal-button swal-button--yes']")
+	WebElement clickyes;
+
+	public Customerfunctionality() {
+		PageFactory.initElements(driver, this);
+
+	}
+
+	public void Customervalidation() throws Throwable {
+		Thread.sleep(2000);
+		clickkeypad.click();
+		Thread.sleep(2000);
+		clickCustomers.click();
+		Thread.sleep(2000);
+		clickAddCustomer.click();
+		Thread.sleep(2000);
+		EnterCustomerName.sendKeys(prop.getProperty("EnterCustomerName"));
+		Thread.sleep(2000);
+		EnterMobileNumber.sendKeys(prop.getProperty("EnterMobileNumber"));
+		Thread.sleep(2000);
+		EnterEmailId.sendKeys(prop.getProperty("EnterEmailId"));
+		EnterWhatsappNumber.sendKeys(prop.getProperty("EnterWhatsappNumber"));
+		Thread.sleep(2000);
+		clickSave.click();
+		Thread.sleep(2000);
+		search.sendKeys(prop.getProperty("CustomerSearch"));
+		Edit.click();
+		EnterCustomerName.clear();
+		EnterCustomerName.sendKeys(prop.getProperty("REEnterCustomerName"));
+		EnterMobileNumber.clear();
+		EnterMobileNumber.sendKeys(prop.getProperty("REEnterMobileNumber"));
+		EnterEmailId.clear();
+		EnterEmailId.sendKeys(prop.getProperty("REEnterEmailId"));
+		EnterWhatsappNumber.clear();
+		EnterWhatsappNumber.sendKeys(prop.getProperty("REEnterWhatsappNumber"));
+		Thread.sleep(2000);
+		clickSave.click();
+		Thread.sleep(2000);
+		Delate.click();
+		Thread.sleep(2000);
+		clickyes.click();
+	}
 
 }
+
+
+
--- Empirehomecrm/src/test/java/com/testcases/customertestcases.java
+++ Empirehomecrm/src/test/java/com/testcases/customertestcases.java
@@ -1,5 +1,51 @@
 package com.testcases;
 
-public class customertestcases {
+import java.beans.Customizer;
 
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.base.Basetest;
+import com.pageobjects.Customerfunctionality;
+import com.pageobjects.Loginfunctionality;
+
+public class customertestcases<Login_page02> extends Basetest {
+
+	Loginfunctionality Login;
+	Customerfunctionality Customer;
+
+	public customertestcases() {
+		super();
+	}
+
+	@BeforeMethod
+	public void setup() {
+
+		initialization();
+
+		Login = new Loginfunctionality();
+		Customer = new Customerfunctionality();
+	}
+
+	@Test
+
+	public void Customervalidation() throws Throwable {
+		Login.verifylogin();
+		Customizer. customervalidation();
+
+		String urltest = driver.getCurrentUrl();
+		AssertJUnit.assertEquals(urltest, "http://empirehome.myprojectsonline.co.in/User/CustomerList");
+
+	}
+
+	@AfterMethod
+	public void teardown() {
+
+	}
 }
+
+ 
+
+
