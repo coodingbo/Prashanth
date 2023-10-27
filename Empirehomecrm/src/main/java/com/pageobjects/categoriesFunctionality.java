d595022d2b4277631d98941defcf2412d6d852f2
SMART IT <SMART IT@DESKTOP-7H9P1AP>
2023-10-27 12:28:52 +0530

    category

--- Empirehomecrm/src/main/java/com/pageobjects/categoriesFunctionality.java
+++ Empirehomecrm/src/main/java/com/pageobjects/categoriesFunctionality.java
@@ -1,5 +1,112 @@
 package com.pageobjects;
 
-public class categoriesFunctionality {
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.base.Basetest;
+
+public class categoriesFunctionality  extends Basetest {
+	
+	@FindBy (xpath = "//span[@class='pe-7s-keypad']")
+	WebElement element;
+	
+	@FindBy (xpath = "//span[normalize-space()='Masters']")
+	WebElement masters;
+	
+	@FindBy (xpath = "//a[normalize-space()='Categories']")
+	WebElement categories;
+	
+	@FindBy (xpath = "//a[normalize-space()='Add New Category']")
+	WebElement addnewcategory;
+	
+	@FindBy (xpath = "//input[@id='CategoryName']")
+	WebElement categoryname;
+	
+	@FindBy (xpath = "//button[normalize-space()='Save']")
+	WebElement savebutton;
+	
+	@FindBy (xpath = "//input[@class='form-control form-control-sm']")
+	WebElement search;
+	
+	@FindBy (xpath = "//select[@name='mydatatable_length']")
+	WebElement tablelength;
+	
+	@FindBy (xpath = "//i[@class='fa fa-pencil']")
+	WebElement edit;
+	
+	@FindBy (id = "preloader")
+	WebElement loader;
+	
+	@FindBy (xpath = "//i[@class='fa fa-trash-o']")
+	WebElement delete;
+	
+	@FindBy (xpath = "//button[@class='swal-button swal-button--yes']")
+	WebElement alert;
+	
+	@FindBy (xpath = "//select[@name='mydatatable_length']")
+	WebElement length;
+	
+	public categoriesFunctionality() {
+		PageFactory.initElements(driver,this);
+	}
+	
+	
+	public void common() throws Throwable {
+		
+		Thread.sleep(2000);
+		element.click();
+		masters.click();
+		categories.click();
+	}	
+		
+	public void verifyaddnewcategory() throws Throwable {	
+		
+		Thread.sleep(2000);
+		addnewcategory.click();
+		categoryname.sendKeys(prop.getProperty("categoryName"));
+		Thread.sleep(2000);
+		savebutton.click();
+		
+	}
+	
+	public void verifyeditcategory() throws Throwable {
+				
+		search.sendKeys(prop.getProperty("categoryName"));
+		search.sendKeys(Keys.ENTER);
+		Thread.sleep(2000);
+
+		edit.click();
+		categoryname.clear();
+		categoryname.sendKeys(prop.getProperty("editcategoryName"));
+		Thread.sleep(2000);
+		savebutton.click();
+		
+		WebElement Edit = driver.findElement(By.xpath("//div[@id='mydatatable_info']"));
+
+	
+		
+		
+	}
+	
+	public void verifydeletecategory() throws Throwable {
+		
+		search.sendKeys(prop.getProperty("editcategoryName"));
+		search.sendKeys(Keys.ENTER);
+		
+		Thread.sleep(2000);
+		delete.click();
+		Thread.sleep(2000);
+		alert.click();
+		
+		
+		
+		
+	}
 
 }
+
+
+
--- Empirehomecrm/src/test/java/com/testcases/categorytest.java
+++ Empirehomecrm/src/test/java/com/testcases/categorytest.java
@@ -1,5 +1,69 @@
 package com.testcases;
 
-public class categorytest {
+import org.openqa.selenium.By;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.util.Assert;
+import com.base.Basetest;
+import com.pageobjects.Loginfunctionality;
+import com.pageobjects.categoriesFunctionality;
+
+public class categorytest extends Basetest {
+	
+	Loginfunctionality lf;
+	categoriesFunctionality ctf;
+	
+	public categorytest() {
+		super();
+	}
+	
+	@BeforeTest
+	public void setup() {
+		initialization();
+		
+		lf = new Loginfunctionality();
+		lf.verifylogin();
+		
+		ctf = new categoriesFunctionality();
+	}
+	
+	
+	@Test (priority = 1)
+	public void addnewcategoryvalidation() throws Throwable {
+		ctf.common();
+		ctf.verifyaddnewcategory();
+		
+		List<WebElement> records = driver.findElements(By.tagName("tr"));
+		int recordcount = records.size();
+	    Assert.assertEquals(recordcount, 8);
+		
+	}
+	
+	@Test (priority = 2)
+	public void editcategoryvalidation() throws Throwable {
+		//ctf.common();
+		ctf.verifyeditcategory();
+		
+		String urltest = driver.getCurrentUrl();
+		Assert.assertEquals(urltest, "http://empirehome.myprojectsonline.co.in/Master/Categories");
+	}
+	
+	@Test (priority = 3)
+	public void deletecategoryvalidation() throws Throwable {
+		
+		ctf.verifydeletecategory();
+		
+		List<WebElement> records = driver.findElements(By.tagName("tr"));
+		int recordcount = records.size();
+	    Assert.assertEquals(recordcount, 7);
+	}
+
+	@AfterTest
+	public void teardown() {
+		
+	}
+	
 
 }
